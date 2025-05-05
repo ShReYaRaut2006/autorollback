bc0fe636e7d7bba9da455c02b3a8b63d4ae6dfd5
ShReYaRaut2006 <161237734+ShReYaRaut2006@users.noreply.github.com>
2025-05-05 15:19:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello():
-    return "🚀 Hello i am shreya .This is version 3 " 
+    return "🚀 Hello i am shreya .This is version 2 " 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8000)
